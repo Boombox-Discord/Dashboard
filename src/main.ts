6f996dba4293ca12513d37fc7a56f13283346165
AjayACST <ajay@acst.co.nz>
2021-08-28 19:46:57 +1200

    Added base vue code

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import "mutationobserver-shim";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+createApp(App).use(router).mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <NavBar name="Boombox Dashboard" />
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    NavBar,
+  },
+};
+</script>
